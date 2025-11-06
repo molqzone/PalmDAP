6ccf43df3f9d9658457fab2272e4afa36432213e
keruth <toiperus@sohu.com>
2025-11-06 20:03:31 +0800

    refactor(app): update USB buffer definitions and improve initialization logic

--- User/app_main.cpp
+++ User/app_main.cpp
@@ -1,37 +1,59 @@
 #include <cmath>
 
 #include "cdc_uart.hpp"
-#include "cdc_test.hpp"
 #include "ch32_gpio.hpp"
+#include "ch32_spi.hpp"
 #include "ch32_timebase.hpp"
 #include "ch32_uart.hpp"
 #include "ch32_usb.hpp"
 #include "ch32_usb_dev.hpp"
 #include "ch32v30x_gpio.h"
-#include "hid_keyboard.hpp"
+#include "dap/hid_dap.hpp"
+#include "dap_io.hpp"
 #include "libxr.hpp"
-#include "semaphore.hpp"
-#include "thread.hpp"
 
-static uint8_t ep0_buffer[64];
-static uint8_t ep1_buffer[256], ep2_buffer[256];
+// EP0: Control, 64 bytes
 static uint8_t ep0_buffer_hs[64];
-static uint8_t ep1_buffer_tx_hs[1024], ep2_buffer_rx_hs[1024], ep3_buffer_tx_hs[1024];
+// EP1: HID IN (64 bytes report size)
+static uint8_t ep1_buffer_tx_hs[1024];
+// EP2: HID OUT (64 bytes report size)
+static uint8_t ep2_buffer_rx_hs[1024];
+// EP3: CDC Data IN
+static uint8_t ep3_buffer_tx_hs[1024];
+// EP4: CDC Data OUT
+static uint8_t ep4_buffer_rx_hs[1024];
+// EP5: CDC Notification IN
+static uint8_t ep5_buffer_tx_hs[8];
+
+uint8_t spi_dma_tx_buffer[64], spi_dma_rx_buffer[64];
 
 extern "C" void app_main()
 {
-  // Initialize USB device
+  LibXR::CH32SPI spi1(CH32_SPI1, {spi_dma_rx_buffer, 64}, {spi_dma_tx_buffer, 64}, GPIOA,
+                      GPIO_Pin_5, GPIOA, GPIO_Pin_6, GPIOA, GPIO_Pin_7);
+
+  LibXR::CH32GPIO gpio_swdio(GPIOA, GPIO_Pin_8);
+  LibXR::CH32GPIO gpio_tdo(GPIOA, GPIO_Pin_9);
+  LibXR::CH32GPIO gpio_nreset(GPIOA, GPIO_Pin_10);
+
+  DAP::DapIo dap_io_instance(spi1, gpio_swdio, gpio_tdo, gpio_nreset);
+
+  LibXR::USB::HIDCmsisDap dap_interface(dap_io_instance);
+
   static constexpr auto LANG_PACK_EN_US = LibXR::USB::DescriptorStrings::MakeLanguagePack(
       LibXR::USB::DescriptorStrings::Language::EN_US, "XRobot", "CDC Demo", "123456789");
 
   LibXR::USB::CDCUart cdc(4096, 4096, 8), cdc1;
-
-  LibXR::CH32USBDeviceFS usb_dev(
+  ;
+  LibXR::CH32USBDeviceFS usb_device(
       /* EP */
       {
-          {ep0_buffer},
-          {ep1_buffer},
-          {ep2_buffer},
+          {ep0_buffer_hs},            // EP0: Control
+          {ep1_buffer_tx_hs, true},   // EP1: HID IN
+          {ep2_buffer_rx_hs, false},  // EP2: HID OUT
+          {ep3_buffer_tx_hs, true},   // EP3: CDC Data IN
+          {ep4_buffer_rx_hs, false},  // EP4: CDC Data OUT
+          {ep5_buffer_tx_hs, true},   // EP5: CDC Notification IN
       },
       /* packet size */
       LibXR::USB::DeviceDescriptor::PacketSize0::SIZE_64,
@@ -40,104 +62,24 @@ extern "C" void app_main()
       /* language */
       {&LANG_PACK_EN_US},
       /* config */
-      {{&cdc1}});
-
-  usb_dev.Init();
-
-  usb_dev.Start();
-
-  LibXR::CH32USBDeviceHS usb_dev_hs(
-      /* EP */
+      // A compound device with two CDC interfaces and one HID interface
       {
-          {ep0_buffer_hs},
-          {ep1_buffer_tx_hs, true},
-          {ep2_buffer_rx_hs, true},
-          {ep3_buffer_tx_hs, false},
-      },
-      /* vid pid bcd */
-      0x1209, 0x0001, 0x0100,
-      /* language */
-      {&LANG_PACK_EN_US},
-      /* config */
-      {{&cdc}});
+          {&dap_interface},
+          {&cdc1},
+      });
 
-  usb_dev_hs.Init();
+  usb_device.Init();
 
-  usb_dev_hs.Start();
+  usb_device.Start();
 
-  // Initialize timebase
+  // Initialize GPIO pins used by SPI and DAP
+  LibXR::CH32GPIO spi_sck(GPIOA, GPIO_Pin_5);   // SPI1 SCK
+  LibXR::CH32GPIO spi_miso(GPIOA, GPIO_Pin_6);  // SPI1 MISO
+  LibXR::CH32GPIO spi_mosi(GPIOA, GPIO_Pin_7);  // SPI1 MOSI
 
   LibXR::CH32Timebase timebase;
 
   LibXR::PlatformInit(3, 8192);
-
-  // Initialize GPIO
-
-  LibXR::CH32GPIO led_b(GPIOB, GPIO_Pin_4);
-  LibXR::CH32GPIO led_r(GPIOA, GPIO_Pin_15);
-  LibXR::CH32GPIO key(GPIOB, GPIO_Pin_3, LibXR::CH32GPIO::Direction::FALL_INTERRUPT,
-                      LibXR::CH32GPIO::Pull::UP, EXTI3_IRQn);
-
-  void (*key_cb_fun)(bool, LibXR::GPIO *) = [](bool, LibXR::GPIO *led)
-  {
-    static bool flag = false;
-    flag = !flag;
-    led->Write(flag);
-  };
-
-  auto key_cb =
-      LibXR::GPIO::Callback::Create(key_cb_fun, reinterpret_cast<LibXR::GPIO *>(&led_r));
-
-  key.RegisterCallback(key_cb);
-
-  // Initialize UART
-
-  uint8_t uart1_tx_buffer[64], uart1_rx_buffer[64];
-
-  static LibXR::CH32UART uart1(CH32_USART2, uart1_rx_buffer, uart1_tx_buffer, GPIOA,
-                               GPIO_Pin_2, GPIOA, GPIO_Pin_3, 0, 25);
-
-  uart1.SetConfig({
-      .baudrate = 115200,
-      .parity = LibXR::UART::Parity::NO_PARITY,
-      .data_bits = 8,
-      .stop_bits = 1,
-  });
-
-  // Initialize Blink Task
-
-  void (*blink_task)(LibXR::GPIO *) = [](LibXR::GPIO *led)
-  {
-    static bool flag = false;
-    if (flag)
-    {
-      flag = false;
-    }
-    else
-    {
-      flag = true;
-    }
-
-    led->Write(flag);
-  };
-
-  auto blink_task_handle =
-      LibXR::Timer::CreateTask(blink_task, reinterpret_cast<LibXR::GPIO *>(&led_b), 1000);
-  LibXR::Timer::Add(blink_task_handle);
-  LibXR::Timer::Start(blink_task_handle);
-
-  // Initialize RamFS and Terminal
-
-  LibXR::RamFS ramfs;
-
-  LibXR::STDIO::read_ = cdc.read_port_;
-  LibXR::STDIO::write_ = cdc.write_port_;
-
-  LibXR::Terminal<> terminal(ramfs);
-
-  auto terminal_task_handle1 = LibXR::Timer::CreateTask(terminal.TaskFun, &terminal, 1);
-  LibXR::Timer::Add(terminal_task_handle1);
-  LibXR::Timer::Start(terminal_task_handle1);
 
   while (1)
   {
